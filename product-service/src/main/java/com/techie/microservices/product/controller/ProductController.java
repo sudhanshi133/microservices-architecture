42b957d66a5e55363af3f55ef50d7aabddc4d6ba
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-02-16 21:27:54 +0530

    adding

--- product-service/src/main/java/com/techie/microservices/product/controller/ProductController.java
+++ product-service/src/main/java/com/techie/microservices/product/controller/ProductController.java
@@ -1,16 +1,21 @@
 package com.techie.microservices.product.controller;
 
 import com.techie.microservices.product.dto.ProductRequest;
+import com.techie.microservices.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest){
-
+        productService.addProduct(productRequest);
     }
 }
--- product-service/src/main/java/com/techie/microservices/product/service/ProductService.java
+++ product-service/src/main/java/com/techie/microservices/product/service/ProductService.java
@@ -1,7 +1,25 @@
 package com.techie.microservices.product.service;
 
+import com.techie.microservices.product.dto.ProductRequest;
+import com.techie.microservices.product.model.Product;
+import com.techie.microservices.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public void addProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+        productRepository.save(product);
+        log.info("product added successfully");
+    }
 }
