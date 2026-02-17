b540e60e891a074f2e4cf81fa0bea4871dc2aff1
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-02-17 08:35:58 +0530

    adding

--- product-service/src/test/java/com/techie/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/techie/microservices/product/ProductServiceApplicationTests.java
@@ -1,18 +1,53 @@
 package com.techie.microservices.product;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.mongodb.MongoDBContainer;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class ProductServiceApplicationTests {
 
+	@Container
+	@ServiceConnection
 	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
 
 	@Test
 	void shouldCreateProduct() {
-	}
+		String requestBody = """
+                {
+                    "name": "iphone-14",
+                    "description": "this is an iphone",
+                    "price": 120.0
+                }""";
 
+		RestAssured.given()
+				.contentType(ContentType.JSON)
+				.body(requestBody)
+				.when()
+				.post("/api/product")
+				.then()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo("iphone-14"))
+				.body("description", Matchers.equalTo("this is an iphone"))
+				.body("price", Matchers.is(120.0f));
+	}
 }
