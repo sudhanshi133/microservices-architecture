766d2c8cad37768c73020dcf002b79a0a108c3b0
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-02-17 08:22:56 +0530

    adding

--- product-service/src/test/java/com/techie/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/techie/microservices/product/ProductServiceApplicationTests.java
@@ -3,13 +3,16 @@ package com.techie.microservices.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.mongodb.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
 	}
 
 }
