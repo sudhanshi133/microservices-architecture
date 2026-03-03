331fd332e96cbe89750efb0549bc5b55a07f9066
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-03-03 19:17:15 +0530

    adding files

--- order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
@@ -8,18 +8,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.mysql.MySQLContainer;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 @WireMockTest
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
+	@Container
 	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
 
 	@LocalServerPort
