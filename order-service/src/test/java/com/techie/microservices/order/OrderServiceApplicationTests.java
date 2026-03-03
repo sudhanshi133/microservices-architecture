481ae638409c3d433f8fa7ec6b74fc3a5700828c
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-03-03 19:10:04 +0530

    adding files

--- order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
@@ -1,17 +1,63 @@
 package com.techie.microservices.order;
 
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import com.techie.microservices.order.stub.InventoryClientStub;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.mysql.MySQLContainer;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 @WireMockTest
 class OrderServiceApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mySQLContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
+                {
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1
+                }
+                """;
+
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+
+		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
 	}
 
 	// todo there are 2 ways to test this out
