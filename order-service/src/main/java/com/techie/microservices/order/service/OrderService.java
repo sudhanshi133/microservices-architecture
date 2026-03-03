d8580518d94bacd89edd5e9565471a681c0f4b42
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-03-03 18:34:20 +0530

    adding files

--- order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/techie/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.techie.microservices.order.service;
 
+import com.techie.microservices.order.client.InventoryClient;
 import com.techie.microservices.order.model.Order;
 import com.techie.microservices.order.repository.OrderRepository;
 import com.techie.microservices.order.dto.OrderRequest;
@@ -13,15 +14,21 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
-    public void placeOrder(OrderRequest orderRequest){
-     //todo map order request and then save
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
-        order.setSkuCode(orderRequest.skuCode());
-
-        orderRepository.save(order);
+    public void placeOrder(OrderRequest orderRequest) {
+        Boolean isInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        if (isInStock) {
+            //todo map order request and then save
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setQuantity(orderRequest.quantity());
+            order.setSkuCode(orderRequest.skuCode());
+            orderRepository.save(order);
+        }
+        else{
+            throw new RuntimeException("the product is not in stock for now , Try again later");
+        }
     }
 }
