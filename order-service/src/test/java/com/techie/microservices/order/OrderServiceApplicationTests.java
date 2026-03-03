1fca9c578f21e51c3a33ef4994a21492ebd04175
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-03-03 18:49:45 +0530

    adding files

--- order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/techie/microservices/order/OrderServiceApplicationTests.java
@@ -3,13 +3,18 @@ package com.techie.microservices.order;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.cloud.contract.wiremock.*;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+	// todo there are 2 ways to test this out
+	// using mockito or wire mock -this is better because it tests the http call as well
 
 }
